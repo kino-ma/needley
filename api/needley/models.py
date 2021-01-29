e82de95d400d773e706a62cee0b53aa105fc0548
kino-ma <kino.ma.ms@gmail.com>
2021-01-29 19:03:06 +0900

    [feat] ユーザと記事のモデルを定義した

---
+++ api/needley/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+
+
+class User(models.Model):
+    # @someones_name
+    name = models.CharField(min_length=1, max_length=30)
+    # Nickname is display name
+    nickname = models.CharField(min_length=1, max_length=20)
+    # Avator is a url icon image url
+    avator = models.URLField(min_length=1, max_length=200)
+
+    # Date when data were created/updated
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Article(models.Model):
+    # The author of this article. This field can be referenced by `article.author`
+    author = models.ForeignKey(
+        User,
+        related_name="author",
+        on_delete=models.CASCADE
+    )
+    # The title of this article
+    title = models.CharField(min_length=1, max_length=100)
+    # Actual content of this article
+    content = models.TextField()
+
+    # Date when data were created/updated
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
